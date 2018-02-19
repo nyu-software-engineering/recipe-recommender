03de3b8b997b38756e3fe1abe48ceddb6eedbeb6
hpedskis <hannah.pedersen.15@gmail.com>
2018-02-19 13:12:16 -0500

    Add database schema

--- Express/app.js
+++ Express/app.js
@@ -9,6 +9,9 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+const db = require( './db.js' );
+//require('./auth');
+const passport = require('passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ Express/db.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const Ingredient = new Schema ({
+  name: String,
+  quantity: Number, //will probably have to change/add this to be a measurement object
+  
+});
+
+const Pantry = new Schema ({
+  ingredients: [Ingredient]
+});
+
+const Recipe = new Schema ({
+  ingredients: [Ingredient],
+  made: Boolean,
+  favorited: Boolean,
+  instructions: String
+});
+
+const User = new Schema ({
+  id: Number,
+  name: String,
+  email: String,
+  recipes: [Recipe],
+  pantry: [Ingredient]
+  
+});
+//keep track of username and password for each user instance
+User.plugin(passportLocalMongoose);
+
+mongoose.model('User', User);
+mongoose.model('Ingredient', Ingredient);
+mongoose.model('Pantry', Pantry);
+mongoose.model('Recipe', Recipe);
+
+/* is the environment variable, NODE_ENV, set to PRODUCTION? NOT SURE IF WE NEED THIS YET
+if (process.env.NODE_ENV == 'PRODUCTION') {
+    // if we're in PRODUCTION mode, then read the configration from a file
+    // use blocking file io to do this...
+    var fs = require('fs');
+    var path = require('path');
+    var fn = path.join(__dirname, 'config.json');
+    var data = fs.readFileSync(fn);
+
+    // our configuration file will be in json, so parse it and set the
+    // conenction string appropriately!
+    var conf = JSON.parse(data);
+    var dbconf = conf.dbconf;
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb://localhost/hw05';
+}
+*/
+
+dbconf = 'mongodb://localhost/hw05';
+mongoose.connect(dbconf);
