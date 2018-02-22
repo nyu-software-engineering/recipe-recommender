a4d712751a1ae1daec69cfa8263ce451e03065ac
hpedskis <hannah.pedersen.15@gmail.com>
2018-02-21 20:27:16 -0500

    add code and paths for login and registration of a user, also added some documentation

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -1,9 +1,78 @@
-let express = require('express');
-let router = express.Router();
+/*
+index.js contains all the paths that aren't specific to a user... aka homepage, login,
+and registration pages.
 
-/* GET home page. */
+res.render points to a corresponding file in views. so res.render('index') looks for 
+index.hbs in /views ... any variables can be passed in through this line as well.
+*/
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const User = mongoose.model('User');
+
+// get home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Recipe Finder' });
+    if(req.user){
+        console.log('session username is ' + req.user.username);
+        res.render('index', {user: req.user, title: 'Recipe Finder' });
+    }else{
+      console.log("no session username");
+        res.render('index', { title: 'Recipe Finder' });
+    }
+
+});
+
+//get registration page on register button click
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+
+//if a user registers (post), create new user object in database
+router.post('/register', function(req, res) {
+    User.register(new User({username:req.body.username, name: req.body.name, email: req.body.email}),
+        req.body.password, function(err, user){
+            if (err) {
+                // NOTE: error? send message back to registration...
+                //TODO: expand this
+                console.log(err);
+                res.render('register',{message:'Your registration information is not valid'});
+            } else {
+                // NOTE: once you've registered, you should be logged in automatically
+                // ...so call authenticate if there's no error
+                passport.authenticate('local')(req, res, function() {
+                    res.render('index', {user: user});
+                });
+            }
+        });
+});
+
+
+//login to account
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+//when a user logins, authenticate and pass info if neccesary
+router.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err,user) {
+        if(user) {
+            // NOTE: using this version of authenticate requires us to
+            // call login manually
+            req.logIn(user, function(err) {
+                res.redirect('/users/authenticated');
+            });
+        } else {
+            res.render('login', {message:'Your login or password is incorrect.'});
+        }
+    })(req, res, next);
+
+});
+
+router.get('/logout', function(req, res){
+    console.log('logging out');
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
--- Express/routes/users.js
+++ Express/routes/users.js
@@ -1,9 +1,27 @@
 let express = require('express');
 let router = express.Router();
+/*
+users.js contains all the paths that ARE specific to a user... aka once they're logged in
+note that the url changes to be /users/PATH 
+*/
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+//just testing to make sure it works
+router.get('/authenticated', function(req, res, next){
+    console.log('inside GET /authenticated');
+        if(req.user) {
+            console.log(req.user);
+            res.render('index', {user: req.user});
+
+        } else {
+            console.log('error');
+            res.render('login', {message:'to see this page, you must have an account. Login or register below'});
+        }
+
 });
 
 module.exports = router;
