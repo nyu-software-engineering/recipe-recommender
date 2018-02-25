d843fa1b56773298c7147fb47f2c55d6ca76edcd
Leila Mardoum <lsmardoum@gmail.com>
2018-02-25 13:57:18 -0500

    Add generate recipe button and website header

--- Express/app.js
+++ Express/app.js
@@ -4,18 +4,26 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-
+let app = express();
+const db = require( './db.js' );
+require('./auth');
 let index = require('./routes/index');
 let users = require('./routes/users');
 
-let app = express();
-const db = require( './db.js' );
-//require('./auth');
 const passport = require('passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
+const session = require('express-session');
+const sessionOptions = {
+    secret: 'secret!',
+    resave: true,
+    saveUninitialized: true
+};
+app.use(session(sessionOptions));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +32,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// NOTE: initialize passport and let it know that we're enabling sessions
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
---
+++ Express/views/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<body>
+<!-- Header -->
+        <header id="header">
+            <div class="inner">
+                <div class="logo">Recipe Finder</div>
+                <nav id="nav">
+                    <!-- <a href="index.html">My Account</a> -->
+                    <a href="logout">Logout</a>
+                </nav>
+            </div>
+        </header>
+
+</body>
