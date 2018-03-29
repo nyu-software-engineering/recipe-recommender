78e1092087a0c2a57a6ac913e7eecb7848b57e91
millyschiff <mas1470@nyu.edu>
2018-03-29 09:56:28 -0400

    updated recipe and ingredients schemas

--- Express/db.js
+++ Express/db.js
@@ -5,7 +5,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Ingredient = new Schema ({
   name: String,
   quantity: Number, //will probably have to change/add this to be a measurement object
-  
+  unit: String
 });
 
 /*const Pantry = new Schema ({
@@ -16,11 +16,14 @@ const Recipe = new Schema ({
   id: Number,
   name: String,
   prepTime: Number,
+  totalTime: Number,
   ingredients: [Ingredient],
   made: Boolean,
   favorited: Boolean,
   directions: [String],
-  nutrition: [String] //check with team to see if this logic is correct
+  cuisine: [String],
+  nutrition: [String],
+  servings: Number
 });
 
 const User = new Schema ({
