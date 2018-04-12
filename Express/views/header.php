434451bc346bbfb621a96947381266c8d208da14
Leila Mardoum <lsmardoum@gmail.com>
2018-04-12 10:22:50 -0400

    Resolve conflicts between materialize framework and old CSS (issue #89)

--- Express/views/header.php
+++ Express/views/header.php
@@ -5,7 +5,6 @@
             <div class="inner">
                 <div class="logo"> <a href="/recipe">Recipe Finder</a></div>
                 <nav id="nav">
-                    <!-- <a href="index.html">My Account</a> -->
                     <a href="logout">Logout</a>
                 </nav>
             </div>
