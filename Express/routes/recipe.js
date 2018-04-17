06eb3f8471237e017083fe6882b2d87370aaeebc
Denisa <dv758@nyu.edu>
2018-04-17 13:25:34 -0400

    add customized image url to hbs with recipes

--- Express/routes/recipe.js
+++ Express/routes/recipe.js
@@ -37,7 +37,7 @@ router.post('/home', function(req, res, next){
         console.log("inside find recipe ", recipe.name);
         res.render('recipe', {recipe: recipe, user: req.user});
 
-        }); 
+        });
     }else {
             console.log('error');
             res.render('login', {message:'to see this page, you must have an account. Login or register below'});
@@ -68,7 +68,7 @@ router.get('/pantry', function(req, res, next) {
 //if a user creates their pantry (post), create ingredient objects in the database
 router.post('/pantry', function(req, res) {
     User.findOne({username: req.user.username}, function (err, user) {
-     
+
         let ingredients = req.body.ingredient; //array of ingredient names
         console.log(req.body.ingredient);
         let toInsert = [];
@@ -81,7 +81,6 @@ router.post('/pantry', function(req, res) {
             //console.log(ing);
             user.pantry.push(ing);
         });
-
         } else {
             let ing = {
                 name: ingredients,
@@ -89,7 +88,7 @@ router.post('/pantry', function(req, res) {
                 }
             user.pantry.push(ing);
         }
-        
+
         //console.log("outside of for loop");
         user.save((err, user) => {
             if(err){
@@ -98,9 +97,9 @@ router.post('/pantry', function(req, res) {
             //console.log("just saved");
             //console.log(user);
                 });
-    
+
         });
-        res.redirect("/recipe/pantry");  
+        res.redirect("/recipe/pantry");
     });
 
 router.get('/delete/:pantry', function(req, res, next){
@@ -129,7 +128,7 @@ router.get('/inventory', function(req, res, next) {
 
 });
 
-router.get('/logout', function(req, res){ 
+router.get('/logout', function(req, res){
     console.log('logging out');
     req.logout();
     res.redirect('/');
