18111273aee6f76129dba0d1c9f0a2ff6900d1cf
Leila Mardoum <lsmardoum@gmail.com>
2018-04-19 15:39:39 -0400

    Testing measures and units

--- Express/routes/recipe.js
+++ Express/routes/recipe.js
@@ -84,43 +84,54 @@ function ingredientInPantry(pantry, ingObj){
 //if a user creates their pantry (post), create ingredient objects in the database
 router.post('/pantry', function(req, res) {
     User.findOne({username: req.user.username}, function (err, user) {
-
+        console.log("inside post /pantry")
         let ingredients = req.body.ingredient; //array of ingredient names
+        let measures = req.body.measure;
+        let units = req.body.unit;
         console.log(req.body.ingredient);
+        console.log(req.body.measure);
+        console.log(req.body.unit);
         let toInsert = [];
         if(ingredients instanceof Array){
-            ingredients.forEach((ele) => {
-            let ing = {
-                name: ele,
-                measure: 3,
-                }
+            // for(let i=0; i < ingredients.length; i++){
+            //     console.log("index: " + i);
+            //     console.log("ingredient: " + ingredients[i]);
+            //     console.log("measure: " + measures[i]);
+            //     console.log("units: " + units[i]);
+            // }
 
-            if (!ingredientInPantry(user.pantry, ing)){
-                user.pantry.push(ing);
-            }
+        //     ingredients.forEach((ele) => {
+        //     let ing = {
+        //         name: ele,
+        //         measure: 3,
+        //         }
+
+        //     if (!ingredientInPantry(user.pantry, ing)){
+        //         user.pantry.push(ing);
+        //     }
             
-        });
+        // });
         } else {
-            let ing = {
-                name: ingredients,
-                measure: 3,
-                }
-            if (!ingredientInPantry(user.pantry, ing)){
-                user.pantry.push(ing);
-            }
+            // let ing = {
+            //     name: ingredients,
+            //     measure: 3,
+            //     }
+            // if (!ingredientInPantry(user.pantry, ing)){
+            //     user.pantry.push(ing);
+            // }
         }
 
         //console.log("outside of for loop");
-        user.save((err, user) => {
-            if(err){
-                console.log(err);
-            }
-            //console.log("just saved");
-            //console.log(user);
-                });
+        // user.save((err, user) => {
+        //     if(err){
+        //         console.log(err);
+        //     }
+        //     //console.log("just saved");
+        //     //console.log(user);
+        //         });
 
         });
-        res.redirect("/recipe/pantry");
+        // res.redirect("/recipe/pantry");
     });
 
 router.post('/pantry/update', function (req, res) {
