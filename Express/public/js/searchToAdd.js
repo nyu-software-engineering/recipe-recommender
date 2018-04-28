70de5d68b6afc003c2d2b9e1e508fa1d26880000
Denisa <dv758@nyu.edu>
2018-04-28 17:23:46 -0400

    Add search to add/search for recipes

---
+++ Express/public/js/searchToAdd.js
@@ -0,0 +1,27 @@
+function search(){
+  // const ingsInput = document.querySelector("#ingredient-input");
+  const ings = document.querySelectorAll("input.filled-in");
+    document.addEventListener('click', function(el){
+      if(el.target.tagName === "SPAN"){
+         el.target.classList.toggle("checkedOff");
+      }
+    });
+
+  document.querySelector("#ingredient-add").addEventListener("keyup", function() {
+    const input = document.getElementById("ingredient-add");
+    const filter = input.value.toLowerCase();
+    const span = document.getElementsByClassName("ingredients-to-pick");
+    const ingInputs = document.querySelectorAll("input");
+   for (let i = 0; i < span.length; i++) {
+     const ing = span[i].getElementsByTagName("span")[0]
+     console.log(ingInputs[i].classList);
+     if (ing.innerHTML.toLowerCase().indexOf(filter) > -1) {
+       span[i].style.display = "";
+     } else{
+       span[i].style.display = "none";
+     }
+   }
+});
+
+}
+document.addEventListener('DOMContentLoaded', search);
