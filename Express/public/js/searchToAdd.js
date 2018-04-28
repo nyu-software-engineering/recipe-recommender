70787dc05074d0990696b2ad770ec0be3b065e25
Denisa <dv758@nyu.edu>
2018-04-28 18:01:35 -0400

    Checked off add boxes and fix search

--- Express/public/js/searchToAdd.js
+++ Express/public/js/searchToAdd.js
@@ -1,7 +1,13 @@
 function search(){
   // const ingsInput = document.querySelector("#ingredient-input");
   const ings = document.querySelectorAll("input.filled-in");
-  
+  document.addEventListener('click', function(el){
+    console.log("heyfsf")
+    if(el.target.tagName === "SPAN"){
+       el.target.classList.toggle("checkedOff");
+    }
+  });
+
   document.querySelector("#ingredient-add").addEventListener("keyup", function() {
     const input = document.getElementById("ingredient-add");
     const filter = input.value.toLowerCase();
@@ -9,10 +15,10 @@ function search(){
     const ingInputs = document.querySelectorAll("input");
    for (let i = 0; i < span.length; i++) {
      const ing = span[i].getElementsByTagName("span")[0]
-     console.log(ingInputs[i].classList);
-     if (ing.innerHTML.toLowerCase().indexOf(filter) > -1) {
+     console.log(ing);
+     if (ing.innerHTML.toLowerCase().indexOf(filter) > -1 && !ing.classList.contains("checkedOff")) {
        span[i].style.display = "";
-     } else{
+    } else if(!ing.classList.contains("checkedOff")){
        span[i].style.display = "none";
      }
    }
